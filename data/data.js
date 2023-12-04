a4226a2013613e557d7ce90d90a893ee0d123bc2
Kazulhu <102474945+Kazulhu@users.noreply.github.com>
2023-12-04 10:39:26 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,17 @@
+objects = [
+    {
+        title: "Photo",
+        description: "This is a photo of someone taking a photo",
+        image: "https://creativepsddownload.com/wp-content/uploads/2017/06/shutterstock-images.jpg"
+    },
+    {
+        title: "Vaporwave",
+        description: "This is a vaporwave background",
+        image: "https://www2.shutterstock.com/blog/wp-content/uploads/sites/5/2018/01/shutterstock_672687319-Converted.jpg"
+    },
+    {
+        title: "Lake",
+        description: "This is a lake",
+        image: "https://www2.shutterstock.com/blog/wp-content/uploads/sites/5/2016/11/EarthAbound_2880x1800_BlogHeader_ImageOnly_2.jpg"
+    }
+]
